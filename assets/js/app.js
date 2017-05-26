444c1b9cb41e4ee90f92b2a2649717703073f367
KarinAAC <karin_alejo@hotmail.com>
2017-05-26 17:12:23 -0500

    subiendo repositorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,18 +8,15 @@ function initMap() {
     position: laboratoriaLima,
     map: map
   });
-  function buscar(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
-  }
 
-  var latitud, longitud;
+
+
+  var latitud, longitud, miUbicacion;
   var funcionExito=function(posicion){
     latitud=posicion.coords.latitude;
     longitud=posicion.coords.longitude;
 
-    var miUbicacion=new google.maps.Marker({
+    miUbicacion= new google.maps.Marker({
       position: {lat:latitud, lng:longitud},
       map:map
     });
@@ -28,11 +25,54 @@ function initMap() {
     map.setCenter({lat:latitud, lng:longitud});
   }
 
-
-
   var funcionError = function(error){
     alert("Tenemos un problema con encontrar tu ubicacion");
   }
+
+  function buscar(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+  }
+
   document.getElementById("encuentrame").addEventListener("click", buscar);
 
+ var inputPartida=document.getElementById("punto-partida");
+  var inputDestino=document.getElementById("punto-destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService, directionDisplay){
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status){
+      if(status==='OK'){
+        document.getElementById("tarifa");
+        var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+        tarifa.classList.remove("none");
+        var costo = distancia*1.75;
+        if (costo<4) {
+          tarifa.innerHTML = "s/. 4";
+        }else{
+          tarifa.innerHTML = "s/." + parseInt(costo);
+        }
+          directionDisplay.setDirections(response);
+          miUbicacion.setMap(null);
+      }else{
+        window.alert('No encontramos ruta');
+      }
+    });
+  }
+
+  directionDisplay.setMap(map);
+  var trazarRuta = function (){
+    calculateAndDisplayRoute(directionsService,directionDisplay);
+  };
+  document.getElementById("trazar-ruta").addEventListener('click',trazarRuta);
 }
